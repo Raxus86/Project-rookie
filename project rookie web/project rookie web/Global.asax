43aa8ac45e52c7fbd5641dea57687880cf9fd162
Christian Ademius <Christian Ademius>
2016-10-13 22:46:57 +0200

    new project added

---
+++ project rookie web/project rookie web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="project_rookie_web.MvcApplication" Language="C#" %>
---
+++ project rookie web/project rookie web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(project_rookie_web.Startup))]
+namespace project_rookie_web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
